f936133a53383cb425fc49ffecea250404f70197
rgfeldman19 <37316275+rgfeldman19@users.noreply.github.com>
2018-03-13 17:46:10 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Rose's cachematrix function
 
-## Write a short comment describing this function
+
+## this function should return a matrix object with the functions, set, get, setinverse and getinverse. same 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      ivs <- NULL
+      set <- function(y) {
+            x <<- y
+            ivs <<- NULL
+      }
+      get <- function() {x}
+      setinverse <- function(inverse) {ivs <<- inverse}
+      getinverse <- function() {ivs}
+      list(set = set, get = get, setinverse = setinverse, 
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function is of the same structure as the cachemean 
 
 cacheSolve <- function(x, ...) {
+      ivs <- x$getinverse()
+      if(!is.null(ivs)){
+            message("getting cached inverse")
+            return(ivs)
+      }
+      data <- x$get()
+      ivs <- solve(data, ...)
+      x$setinverse(ivs)
+      ivs
         ## Return a matrix that is the inverse of 'x'
 }
